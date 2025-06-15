d0c15ea64e8485aee26d5454efdff52b66ca13d1
Felipe Gonçalves <lipesouzago4@gmail.com>
2025-06-15 02:54:10 -0400

    ajuste logo e nome

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import ChecklistPage from './pages/ChecklistPage.jsx';
-// import ChecklistPage from './pages/ChecklistPage'; // Vamos usar depois
+
 import './App.css';
 
 function App() {
@@ -9,12 +9,12 @@ function App() {
     <div className="App">
       <header className="app-header">
         <img className='img-header' src="./LOGO.png" alt="" />
-        <h1>Checklist Virtual</h1>
+        <h1>OfiCheck</h1>
       </header>
       <main className="app-main">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* A rota abaixo será para ver/editar um checklist específico */}
+        
           <Route path="/checklist/:id" element={<ChecklistPage />} />
         </Routes>
       </main>
